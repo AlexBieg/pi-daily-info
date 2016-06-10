cc4ee4d498bd4d29a4fd5d3893ab3a4c04162e3d
AlexBieg <alexbieg95@gmail.com>
2016-06-10 14:12:03 -0700

    added current weather summary and location

--- script.js
+++ script.js
@@ -5,53 +5,55 @@ var CLIENT_ID = '492257460870-9hevog7tfv04aqtmflp58eu2th5vst0o.apps.googleuserco
 var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly", "https://www.googleapis.com/auth/gmail.readonly"];
 
 /**
-* Check if current user has authorized this application.
-*/
+ * Check if current user has authorized this application.
+ */
 function checkAuth() {
-    gapi.auth.authorize(
-      {
+    gapi.auth.authorize({
         'client_id': CLIENT_ID,
         'scope': SCOPES.join(' '),
         'immediate': true
-      }, handleAuthResult);
+    }, handleAuthResult);
 }
 
 /**
-* Handle response from authorization server.
-*
-* @param {Object} authResult Authorization result.
-*/
+ * Handle response from authorization server.
+ *
+ * @param {Object} authResult Authorization result.
+ */
 function handleAuthResult(authResult) {
     var authorizeBtn = $('#authorize');
     if (authResult && !authResult.error) {
-      // Hide auth UI, then load client library.
-      authorizeBtn.hide();
-      $('.content').show();
-      loadApis();
+        // Hide auth UI, then load client library.
+        authorizeBtn.hide();
+        $('.content').show();
+        loadApis();
     } else {
-      // Show auth UI, allowing the user to initiate authorization by
-      // clicking authorize button.
-      authorizeBtn.show();
-      $('.content').hide();
+        // Show auth UI, allowing the user to initiate authorization by
+        // clicking authorize button.
+        authorizeBtn.show();
+        $('.content').hide();
     }
 }
 
 /**
-* Initiate auth flow in response to user clicking authorize button.
-*
-* @param {Event} event Button click event.
-*/
+ * Initiate auth flow in response to user clicking authorize button.
+ *
+ * @param {Event} event Button click event.
+ */
 function handleAuthClick(event) {
-    gapi.auth.authorize(
-      {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-      handleAuthResult);
+    gapi.auth.authorize({
+            client_id: CLIENT_ID,
+            scope: SCOPES,
+            immediate: false
+        },
+        handleAuthResult);
     return false;
 }
 
 /**
-* Load Google Calendar client library. List upcoming events
-* once client library is loaded.
-*/
+ * Load Google Calendar client library. List upcoming events
+ * once client library is loaded.
+ */
 function loadApis() {
     gapi.client.load('calendar', 'v3', listUpcomingEvents);
     gapi.client.load('gmail', 'v1', listEmails);
@@ -70,7 +72,7 @@ function listEmails() {
         for (var i = 0; i < messages.length; i++) {
             var messageRequest = gapi.client.gmail.users.messages.get({
                 'userId': 'me',
-                'id' : messages[i].id
+                'id': messages[i].id
             });
 
             messageRequest.execute(function(message) {
@@ -100,64 +102,64 @@ function appendEmail(fromAddress, title) {
 }
 
 /**
-* Print the summary and start datetime/date of the next ten events in
-* the authorized user's calendar. If no events are found an
-* appropriate message is printed.
-*/
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
 function listUpcomingEvents() {
     var today = new Date();
     var endOfToday = new Date();
     endOfToday.setHours(23, 59, 59, 999);
 
     var request = gapi.client.calendar.events.list({
-      'calendarId': 'primary',
-      'timeMin': today.toISOString(),
-      'timeMax': endOfToday.toISOString(),
-      'showDeleted': false,
-      'singleEvents': true,
-      'maxResults': 10,
-      'orderBy': 'startTime'
+        'calendarId': 'primary',
+        'timeMin': today.toISOString(),
+        'timeMax': endOfToday.toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
     });
 
     request.execute(function(resp) {
-      var events = resp.items;
+        var events = resp.items;
 
-      if (events.length > 0) {
-        for (i = 0; i < events.length; i++) {
-            var event = events[i];
-            var when = event.start.dateTime;
-            if (!when) {
-                when = event.start.date;
-            }
-            var whenDate = new Date(when);
-            var formattedHours = whenDate.getHours();
-            var dayPeriod = "AM";
-            if (whenDate.getHours() > 12){
-                formattedHours = whenDate.getHours() - 12;
-                if (whenDate.getHours() != 24 || whenDate.getHours() != 0 || whenDate.getHours() == 12) {
-                    dayPeriod = "PM"
+        if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime;
+                if (!when) {
+                    when = event.start.date;
                 }
+                var whenDate = new Date(when);
+                var formattedHours = whenDate.getHours();
+                var dayPeriod = "AM";
+                if (whenDate.getHours() > 12) {
+                    formattedHours = whenDate.getHours() - 12;
+                    if (whenDate.getHours() != 24 || whenDate.getHours() != 0 || whenDate.getHours() == 12) {
+                        dayPeriod = "PM"
+                    }
+                }
+                var formattedMinutes = whenDate.getMinutes() + "";
+                if (formattedMinutes.length < 2) {
+                    formattedMinutes = "0" + formattedMinutes;
+                }
+                var formatedTime = formattedHours + ":" + formattedMinutes + " " + dayPeriod;
+                appendEvent(formatedTime + ': ' + event.summary, event.htmlLink);
             }
-            var formattedMinutes = whenDate.getMinutes()+"";
-            if (formattedMinutes.length < 2) {
-                formattedMinutes = "0" + formattedMinutes;
-            }
-            var formatedTime = formattedHours + ":" + formattedMinutes + " " + dayPeriod;
-            appendEvent(formatedTime + ': ' + event.summary, event.htmlLink);
+        } else {
+            appendEvent('No upcoming events found.', '#');
         }
-      } else {
-        appendEvent('No upcoming events found.', '#');
-      }
 
     });
 }
 
 /**
-* Append a pre element to the body containing the given message
-* as its text node.
-*
-* @param {string} message Text to be placed in pre element.
-*/
+ * Append a pre element to the body containing the given message
+ * as its text node.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
 function appendEvent(message, link) {
     var list = $('.event-list');
     var item = $('<li>');
@@ -171,20 +173,46 @@ function appendEvent(message, link) {
 }
 
 function loadNews() {
-  //   $.ajax({
-  //   url: "https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&topic=tc&output=rss",
-  //   jsonp: "callback",
-  //
-  //   // tell jQuery we're expecting JSONP
-  //   dataType: "jsonp",
-  //   success: function( response ) {
-  //     console.log(response);
-  //   }
-  // });
+    //   $.ajax({
+    //   url: "https://news.google.com/news?cf=all&hl=en&pz=1&ned=us&topic=tc&output=rss",
+    //   jsonp: "callback",
+    //
+    //   // tell jQuery we're expecting JSONP
+    //   dataType: "jsonp",
+    //   success: function( response ) {
+    //     console.log(response);
+    //   }
+    // });
 }
 
-function loadWeather() {
-    
+function loadWeather(pos) {
+    $.getJSON("https://api.forecast.io/forecast/c8a59ebce21645ec34d8dd31d21d15e2/" + pos.lat + "," + pos.lng + "?callback=?", function(response) {
+        console.log(response);
+        //add current weather
+        var currentP = $("<p>");
+        currentP.text(response.hourly.summary);
+        $(".current-weather").append(currentP);
+    })
+}
+
+function findLocation() {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            console.log(pos);
+            loadWeather(pos);
+        }, function() {
+            console.log("Error");
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        //handleLocationError(false, infoWindow, map.getCenter());
+        console.log("Location Not supported by Browser");
+    }
 }
 
 
@@ -193,6 +221,5 @@ $(function() {
         handleAuthClick();
     });
 
-    loadNews();
-    loadWeather();
+    //loadNews();
 });
